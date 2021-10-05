fb38ecf753d2ffb62f95b5dc323f6ecd4d6b1930
SirRGB <liam_wagner@gmx.de>
2021-10-05 16:54:41 +0200

    blissify

--- bliss_dumpling.mk
+++ bliss_dumpling.mk
@@ -27,9 +27,9 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Inherit some common DotOS stuff.
-$(call inherit-product, vendor/dot/config/common.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
-PRODUCT_NAME := dot_dumpling
+PRODUCT_NAME := bliss_dumpling
 PRODUCT_DEVICE := dumpling
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
